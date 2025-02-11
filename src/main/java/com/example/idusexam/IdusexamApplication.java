b46e4ec1cc710ac14de9ea32a922e31b94b38a81
scy <tjcksdud0221@naver.com>
2025-02-11 10:09:30 +0900

    initial commit

---
+++ src/main/java/com/example/idusexam/IdusexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.idusexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdusexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdusexamApplication.class, args);
+    }
+
+}
